411febf2983565de0f1b8c0b5d729c463ce2b087
kavinkumar23 <sachinkavin444@gmail.com>
2022-04-06 12:46:16 +0530

    kavin

---
+++ src/kavin/mongodbtutorial/converter/ProductConverter.java
@@ -0,0 +1,29 @@
+package kavin.mongodbtutorial.converter;
+ 
+import org.bson.Document;
+import org.bson.types.ObjectId;
+ 
+import kavin.mongodbtutorial.model.Product;
+ 
+public class ProductConverter {
+ 
+    // convert Product Object to MongoDB Document
+    // take special note of converting id String to ObjectId
+    public static Document toDocument(Product p) {
+        Document doc = new Document("name", p.getName()).append("price", p.getPrice());
+        if (p.getId() != null) {
+            doc.append("_id", new ObjectId(p.getId()));
+        }
+        return doc;
+    }
+ 
+    // convert MongoDB Document to Product
+    // take special note of converting ObjectId to String
+    public static Product toProduct(Document doc) {
+        Product p = new Product();
+        p.setName((String) doc.get("name"));
+        p.setPrice((Double) doc.get("price"));
+        p.setId(((ObjectId) doc.get("_id")).toString());
+        return p;
+    }
+}
